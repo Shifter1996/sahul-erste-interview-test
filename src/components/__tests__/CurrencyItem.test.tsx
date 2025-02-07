d01e129cf195aff97287bc8877150993d16c95bc
SuspiciousCloud <dusansahul@gmail.com>
2025-02-07 11:57:24 +0100

    added react-testing-library to vitest + basic CurrencyItem test

--- src/components/__tests__/CurrencyItem.test.tsx
+++ src/components/__tests__/CurrencyItem.test.tsx
@@ -1,29 +1,21 @@
 import { describe, it, expect } from "vitest"
-// import { render, screen } from "@testing-library/react"
-// import CurrencyItem from "../CurrencyItem"
+import { render, screen } from "@testing-library/react"
+import CurrencyItem from "../CurrencyItem"
 
 describe("CurrencyItem", () => {
   const mockCurrencyRate = {
-    currency: "USD",
-    precision: 2,
+    code: "USD",
+    name: 2,
     nameI18N: "United States Dollar",
-    exchangeRate: { buy: 2.515 },
+    exchangeRate: { buy: 2.512 },
   }
 
   it("renders currency information correctly", () => {
-    expect(1+1).toBe(2);
-    // render(<CurrencyItem currencyRate={mockCurrencyRate} baseCurrency="EUR" />)
-    // expect(screen.getByText("United States Dollar")).toBeInTheDocument()
-    // expect(screen.getByText("USD")).toBeInTheDocument()
-    // expect(screen.getByText("2.51")).toBeInTheDocument()
-    // expect(screen.getByText("EUR"))
+    render(<CurrencyItem currencyRate={mockCurrencyRate} baseCurrency="EUR" />)
+    expect(screen.getByText("United States Dollar")).toBeInTheDocument()
+    expect(screen.getByText("USD")).toBeInTheDocument()
+    expect(screen.getByText("2.51")).toBeInTheDocument()
+    expect(screen.getByText("EUR"))
   })
-
-  // it("renders the flag image", () => {
-  //   render(<CurrencyItem currencyRate={mockCurrencyRate} baseCurrency="EUR" />)
-  //   const flagImage = screen.getByAltText("USD")
-  //   expect(flagImage).toBeInTheDocument()
-  //   expect(flagImage).toHaveAttribute("src", "/placeholder.svg?height=30&width=45&text=USD")
-  // })
 })
 
